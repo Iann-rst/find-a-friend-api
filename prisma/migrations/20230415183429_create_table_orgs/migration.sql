68a6b6cf678ea7321fb0b5193d9cd7de314ffdf2
Iann Rodrigues <iannrst11@gmail.com>
2023-04-15 15:35:30 -0300

    feat: Criando tabela orgs

---
+++ prisma/migrations/20230415183429_create_table_orgs/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE "orgs" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "address" TEXT NOT NULL,
+    "cep" TEXT NOT NULL,
+    "whatsapp" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "orgs_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "orgs_email_key" ON "orgs"("email");
