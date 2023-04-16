c1d678ca815befea483f100077e2cb63a28a3a03
Iann Rodrigues <iannrst11@gmail.com>
2023-04-16 16:07:12 -0300

    feat: Rota para listar todos pets de uma cidade

--- src/http/controllers/pets/routes.ts
+++ src/http/controllers/pets/routes.ts
@@ -1,7 +1,9 @@
 import { verifyJwt } from '@/http/middlewares/verify-jwt'
 import { FastifyInstance } from 'fastify'
 import { register } from './register'
+import { search } from './search'
 
 export async function petsRoutes(app: FastifyInstance) {
   app.post('/register', { onRequest: [verifyJwt] }, register)
+  app.get('/cities/:city', search)
 }
