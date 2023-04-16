af0b0bff1bc000d2d5379a4b2021553eb32441c1
Iann Rodrigues <iannrst11@gmail.com>
2023-04-16 11:50:30 -0300

    feat: Adc rota para refresh_token

--- src/http/controllers/org/routes.ts
+++ src/http/controllers/org/routes.ts
@@ -1,8 +1,14 @@
 import { FastifyInstance } from 'fastify'
 import { authenticate } from './authenticate'
+import { refresh } from './refresh'
 import { register } from './register'
 
 export async function orgRoutes(app: FastifyInstance) {
   app.post('/register', register)
+
+  // (login)
   app.post('/session', authenticate)
+
+  // refresh-token
+  app.patch('/token/refresh', refresh)
 }
