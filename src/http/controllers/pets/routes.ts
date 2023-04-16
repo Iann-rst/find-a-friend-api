ae7cb77ecd27bf33b7ae88656c2e86f9710711b3
Iann Rodrigues <iannrst11@gmail.com>
2023-04-16 19:55:20 -0300

    feat: Rota para buscar detalhes de um pet

--- src/http/controllers/pets/routes.ts
+++ src/http/controllers/pets/routes.ts
@@ -1,9 +1,11 @@
 import { verifyJwt } from '@/http/middlewares/verify-jwt'
 import { FastifyInstance } from 'fastify'
+import { details } from './details'
 import { register } from './register'
 import { search } from './search'
 
 export async function petsRoutes(app: FastifyInstance) {
   app.post('/register', { onRequest: [verifyJwt] }, register)
   app.get('/cities/:city', search)
+  app.get('/:pet_id', details)
 }
