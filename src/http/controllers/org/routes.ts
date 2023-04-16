7ae54f697cee06e565ae8d591b011e9a7614f533
Iann Rodrigues <iannrst11@gmail.com>
2023-04-16 10:06:30 -0300

    feat: Rota para autenticação

--- src/http/controllers/org/routes.ts
+++ src/http/controllers/org/routes.ts
@@ -1,6 +1,8 @@
 import { FastifyInstance } from 'fastify'
+import { authenticate } from './authenticate'
 import { register } from './register'
 
 export async function orgRoutes(app: FastifyInstance) {
   app.post('/register', register)
+  app.post('/session', authenticate)
 }
