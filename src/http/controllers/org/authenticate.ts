35612d596f8df779cdc4f1de65d6ca004e8c6fcb
Iann Rodrigues <iannrst11@gmail.com>
2023-04-16 10:06:16 -0300

    feat: Controller para autenticação de uma Org

---
+++ src/http/controllers/org/authenticate.ts
@@ -0,0 +1,35 @@
+import { PrismaOrgRepository } from '@/repositories/prisma/prisma-org-repository'
+import { AuthenticateOrgUseCase } from '@/use-cases/authenticate-org'
+import { InvalidCredentialsError } from '@/use-cases/errors/invalid-credentials-error'
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+const authenticateBodySchema = z.object({
+  email: z.string().email(),
+  password: z.string(),
+})
+
+export async function authenticate(
+  request: FastifyRequest,
+  reply: FastifyReply,
+) {
+  try {
+    const { email, password } = authenticateBodySchema.parse(request.body)
+
+    const orgRepository = new PrismaOrgRepository()
+    const authenticateOrgUseCase = new AuthenticateOrgUseCase(orgRepository)
+
+    const { org } = await authenticateOrgUseCase.execute({ email, password })
+
+    // TODO: Utilizar JWT e retornar apenas o token
+    return reply.status(200).send(org)
+  } catch (error) {
+    if (error instanceof InvalidCredentialsError) {
+      return reply.status(400).send({
+        message: error.message,
+      })
+    }
+
+    throw error
+  }
+}
